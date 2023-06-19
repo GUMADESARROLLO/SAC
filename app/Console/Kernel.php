235f135a629f221e85170150ba69b3db8e5d2770
Guma Master <analista.guma@gmail.com>
2023-06-19 15:53:39 -0600

    Feat: Insert pedidos

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,7 +32,7 @@ class Kernel extends ConsoleKernel
         $schedule->call(function () {
             $scheduleController = new ScheduleController();
             $scheduleController->RunPedidos();
-        })->everyThirtyMinutes();
+        })->everyFiveMinutes();
     }
 
     /**
