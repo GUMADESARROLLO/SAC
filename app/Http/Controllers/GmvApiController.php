ec551a69cffeddfe1d9f2f2cc13d148675dfc1c4
Guma Desarrollo <ANALISTA.GUMA@GMAIL.COM>
2024-02-04 11:29:49 -0600

    :pencil2: [fix] FIX TYPO

--- app/Http/Controllers/GmvApiController.php
+++ app/Http/Controllers/GmvApiController.php
@@ -297,7 +297,7 @@ class GmvApiController extends Controller{
         return response()->json($Comision);
     }
 
-    public static function CronCheckVisita(){
+    public static function runCronCheckVisita(){
         $visita = Visita::validarVisita();
         return response()->json($visita);
     }
